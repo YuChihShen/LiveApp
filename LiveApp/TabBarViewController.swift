9a4f56f94d28f521eec6454304218f822ddde8ad
yu-chih <yu-chih@mail.chungyo.net>
2022-04-19 17:53:50 +0800

    final test

--- LiveApp/TabBarViewController.swift
+++ LiveApp/TabBarViewController.swift
@@ -8,36 +8,38 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-    var item1 : UIImageView!
-    var item2 : UIImageView!
-    var item3 : UIImageView!
+    var img1 :UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let item1 = tabBar.subviews.first?.subviews.first as? UIImageView
-        //        item1 = tabBar.subviews.first as? UIImageView
+//        var imageV = self.tabBar.subviews[0]
+//        self.img1 = imageV.subviews[0] as! UIImageView
+//        item1 = tabBar.subviews.first as? UIImageView
 //        item1.contentMode = .center
 //        item1.tag = 0
 //        item2 = tabBar.subviews[1] as? UIImageView
 //        item3 = tabBar.subviews[2] as? UIImageView
-      
-        
     }
+        
+    
     
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        print(self.item1)
-//        item1 = tabBar.subviews[0] as? UIImageView
-//        item2 = tabBar.subviews[1] as? UIImageView
-//        item3 = tabBar.subviews[2] as? UIImageView
+//        self.cabasicanimation.fillMode = kCAFillModeForwards;
+//        self.cabasicanimation.removedOnCompletion = NO
+//        let animation = CABasicAnimation
+        self.animate(tag: item.tag)
+//        let item1 = tabBar.subviews.first
+//        let itemitem = item1?.subviews.first
 //        if item.tag == 1{
-////        let selectedImage = UIImageView(image: tabBar.selectedItem?.selectedImage)
-////        let image = UIImageView(image: tabBar.selectedItem?.image)
-////        tabBar.addSubview(selectedImage)
-//
-//        UIView.animate(withDuration: 0.5, delay: 0, animations: {
-//            self.item1.transform = CGAffineTransform(rotationAngle: .pi)
-//        })
-//        }
-        
+//            UIView.animate(withDuration: 1.5, delay: 0, animations: {
+//                self.img1.transform = CGAffineTransform(rotationAngle: .pi * 1)
+//                    })
+        }
+    
+    func animate(tag:Int){
+        var imageV = self.tabBar.subviews.first
+        img1 = imageV!.subviews[tag] as? UIImageView
+        UIView.animate(withDuration: 1.5, delay: 0, animations: {
+            self.img1.transform = CGAffineTransform(rotationAngle: .pi * 1)})
     }
-
+    
 }
